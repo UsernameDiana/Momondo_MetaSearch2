ed09f48bddcc40f7c5943e3d551f682d74bd259e
Diana <Diana@10.50.137.157>
2016-11-23 12:34:43 +0100

    added html page

---
+++ src/main/webapp/js/app.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var app = angular.module('MomondoMetaApp', []);
+
+app.controller('SearchCtrl', function($scope){
+    
+    $scope.startDate= {};
+    $scope.endDate = {};
+    
+});
+
+
