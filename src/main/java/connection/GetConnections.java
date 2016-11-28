74e168a965fc03f366f394933c19a494a516e797
Jarmo Hakkarainen <Jarmo Hakkarainen>
2016-11-28 10:22:55 +0100

    Fixed the getconnections class

--- src/main/java/connection/GetConnections.java
+++ src/main/java/connection/GetConnections.java
@@ -17,99 +17,62 @@ import java.net.URL;
  * @author jarmo
  */
 public class GetConnections {
-    
+
     public GetConnections() {
     }
+
     public static void main(String[] args) {
+        
         try {
-                //Connection string for now to lars's airline, final product in list
-		URL url = new URL("http://airline-plaul.rhcloud.com/api/flightinfo/CPH/2017-01-30T00:00:00.000Z/1");
-                //Creating the connection
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                //Specifying the request type
-		conn.setRequestMethod("GET");
-                //Setting correct headers
-		conn.setRequestProperty("Accept", "application/json");
+            
+            //Connection string for now to lars's airline, final product in list
+            URL url = new URL("http://airline-plaul.rhcloud.com/api/flightinfo/CPH/2017-01-30T00:00:00.000Z/1");
+            
+            //Creating the connection
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            
+            //Specifying the request type
+            conn.setRequestMethod("GET");
+            
+            //Setting correct headers
+            conn.setRequestProperty("Accept", "application/json");
 //                //Checking if the server is available
 //		if (conn.getResponseCode() != 200) {
 //			throw new RuntimeException("Failed : HTTP error code : "
 //					+ conn.getResponseCode());
 //		}
-                //Getting the actual input from the external server
-		BufferedReader br = new BufferedReader(new InputStreamReader(
-			(conn.getInputStream())));
+            
+            //Getting the actual input from the external server
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
 
-		String output;
-		System.out.println("Output from Server .... \n");
-                //Storing the server inbound 
-		output = br.readLine();
-			         System.out.println("Result: " +output);
-                //Kill the connection
-		conn.disconnect();
-                //Final return, is a string
-                System.out.println("Final thing:" + output);
+            String output;
+            StringBuffer response = new StringBuffer();
 
-	  } catch (MalformedURLException e) {
+            //Storing the server inbound 
+            while ((output = br.readLine()) != null) {
+                response.append(output);
+            }
 
-                System.out.println("error occured " + e); 
+            System.out.println("Output from Server .... \n");
 
-	  } catch (IOException e) {
+            System.out.println("Result: " + response);
 
-                System.out.println("error occured " + e);
+            //Kill the connection
+            conn.disconnect();
 
-	  }
+            //Final return, is a string
+            System.out.println("Final thing:" + response);
+
+        } catch (MalformedURLException e) {
+
+            System.out.println("error occured " + e);
+
+        } catch (IOException e) {
+
+            System.out.println("error occured " + e);
+
+        }
     }
-    //Method for the most basic query with three parameters
-    //The arguments in the method come through the rest api
-//    public void GetWithThree() {
-//    
-//        
-//        
-//        try {
-//                //Connection string for now to lars's airline, final product in list
-//		URL url = new URL("http://airline-plaul.rhcloud.com/api/flightinfo/CPH/2017-01-30T00:00:00.000Z/1");
-//                //Creating the connection
-//		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//                //Specifying the request type
-//		conn.setRequestMethod("GET");
-//                //Setting correct headers
-//		conn.setRequestProperty("Accept", "application/json");
-////                //Checking if the server is available
-////		if (conn.getResponseCode() != 200) {
-////			throw new RuntimeException("Failed : HTTP error code : "
-////					+ conn.getResponseCode());
-////		}
-//                //Getting the actual input from the external server
-//		BufferedReader br = new BufferedReader(new InputStreamReader(
-//			(conn.getInputStream())));
-//
-//		String output;
-//		System.out.println("Output from Server .... \n");
-//                //Storing the server inbound 
-//		output = br.readLine();
-//			         System.out.println("Result: " +output);
-//                //Kill the connection
-//		conn.disconnect();
-//                //Final return, is a string
-//                System.out.println(output);
-//
-//	  } catch (MalformedURLException e) {
-//
-//                System.out.println("error occured " + e); 
-//
-//	  } catch (IOException e) {
-//
-//                System.out.println("error occured " + e);
-//
-//	  }
-//        
-//        
-//          
-//	}
+
 }
-        
-       
-    
-
-
-
