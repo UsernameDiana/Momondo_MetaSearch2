8498f78f7185996ef6ba92e68ea468bd74c9575a
Jarmo Hakkarainen <Jarmo Hakkarainen>
2016-12-01 10:17:48 +0100

    Changes to the angular and html. THe url is now properly formatted

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -20,7 +20,7 @@ app.config(function ($routeProvider) {
             })
             .otherwise({
                 redirectTo: "/index"
-            })
+            });
 });
 
 app.controller('DocController', function ($scope) {
@@ -34,24 +34,41 @@ app.controller('AboutController', function ($scope) {
 app.controller('SearchCtrl', function ($scope, $http) {
     $scope.flight = {};
 
+
+
     $scope.search4Param = function () {
         $http({
             url: 'http://airline-plaul.rhcloud.com/api/flightinfo/' + $scope.flight.startPlace
-                    + "/" + $scope.flight.date + "/" + $scope.flight.passangers
+                    + "/" + $scope.date + "/" + $scope.flight.passangers
                     + "/" + $scope.flight.endPlace,
-            method: 'GET',
+            method: 'GET'
         }).then(function (response, $scope) {
             $scope.flights = response;
         });
     };
 
     $scope.search3Param = function () {
+        
+        var date;        
+        date = new Date($scope.myDate);
+        var dateTemp = date.getTime() - (date.getTimezoneOffset() * 60000);
+        var adjustedDate = new Date(dateTemp).toISOString();
+        
+        JSON.stringify($scope.date);
         $http({
-            url: 'http://airline-plaul.rhcloud.com/api/flightinfo/' + $scope.flight.startPlace
-                    + "/" + $scope.flight.date + "/" + $scope.flight.passangers,
             method: 'GET',
+            url: 'http://airline-plaul.rhcloud.com/api/flightinfo/' + $scope.flight.startPlace
+                    + "/" + adjustedDate + "/" + $scope.flight.passangers,
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+            
         }).then(function (response, $scope) {
-            $scope.flights = response;
+            $scope.status = response;
+            $scope.flights = JSON.stringify(response.data, null, 2);
+        }, function error(response) {
+            $scope.status = response.status;
+            $scope.flights = JSON.stringify(response.data, null, 2);
         });
     };
 });
