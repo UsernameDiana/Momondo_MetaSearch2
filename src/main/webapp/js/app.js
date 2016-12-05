deeb32646f0ace6497079bd4f7266c18b720c105
Jarmo Hakkarainen <Jarmo Hakkarainen>
2016-12-05 11:41:33 +0100

    Did the change to views and tried some styling on the flightlist

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -7,9 +7,13 @@ app.config(function ($routeProvider) {
             .when("/home", {
                 templateUrl: "index.html",
                 controller: "SearchCtrl"
-            })                            
+            })
             .when("/info/:index", {
                 templateUrl: "views/viewFlightInfo.html",
+                controller: "SearchCtrl"
+            })
+            .when("/flightlist", {
+                templateUrl: "views/flightList.html",
                 controller: "SearchCtrl"
             })
             .otherwise({
@@ -26,51 +30,49 @@ app.controller('AboutController', function ($scope) {
 });
 
 app.controller('SearchCtrl', function ($scope, $http, $routeParams, $location) {
-        
+
     $scope.search3Param = function () {
-        
-        var date;        
+
+        var date;
         date = new Date($scope.myDate);
         var dateTemp = date.getTime() - (date.getTimezoneOffset() * 60000);
         var adjustedDate = new Date(dateTemp).toISOString();
-        
+
         JSON.stringify($scope.date);
         if ($scope.flight.endPlace === null) {
             $http({
-            method: 'GET',
-            url: 'api/search/' + $scope.flight.startPlace
-                    + "/" + adjustedDate + "/" + $scope.flight.passangers            
-        }).then(function (response) {  
-            
-            $scope.result = response.data;            
-            $scope.flights = response.data.flights;
-            console.log($scope.result);
-            console.log($scope.flights);
-        });
-        }
-        
-        else {
+                method: 'GET',
+                url: 'api/search/' + $scope.flight.startPlace
+                        + "/" + adjustedDate + "/" + $scope.flight.passangers
+            }).then(function (response) {
+
+                $scope.result = response.data;
+                $scope.flights = response.data.flights;
+                console.log($scope.result);
+                console.log($scope.flights);
+            });
+        } else {
             $http({
-            method: 'GET',
-            url: 'api/search/' + $scope.flight.startPlace + '/' + $scope.flight.destination
-                    + "/" + adjustedDate + "/" + $scope.flight.passangers            
-        }).then(function (response) {  
-            
-            $scope.result = response.data;            
-            $scope.flights = response.data.flights;
-            console.log($scope.result);
-            console.log($scope.flights);
-        });
+                method: 'GET',
+                url: 'api/search/' + $scope.flight.startPlace + '/' + $scope.flight.destination
+                        + "/" + adjustedDate + "/" + $scope.flight.passangers
+            }).then(function (response) {
+
+                $scope.result = response.data;
+                $scope.flights = response.data.flights;
+                console.log($scope.result);
+                console.log($scope.flights);
+            });
         }
 
-        
+
     };
-    $scope.changeView = function(path) {
+    $scope.changeView = function (path) {
         $location.path(path);
     };
-    
+
     if (angular.isDefined($routeParams.index)) {
-    var i = $routeParams.index;
-    $scope.flights = $scope.flights[i];
-  }
+        var i = $routeParams.index;
+        $scope.flights = $scope.flights[i];
+    }
 });
